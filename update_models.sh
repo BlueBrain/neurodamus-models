be4ad5fc8189cba2b9e44be05824cd02ee9cd4e1
Fernando Pereira <fernando.pereira@epfl.ch>
2021-11-26 15:01:13 +0000

    Automation to automatically bump models
    This PR implements pipeline jobs which automate the process of bumping its own submodules when any of them gets updated.
    
    Basically submodules CI should include the `_ci/test_models.yml` pipeline, which besides basic tests, will trigger a job in this very self project that creates a new commit, push and opens MR. Following that, the other normal pipeline jobs will run (which for the moment are a stub). See the test MR: https://bbpgitlab.epfl.ch/hpc/sim/neurodamus-models/-/merge_requests/3 which was created by pipeline https://bbpgitlab.epfl.ch/hpc/sim/models/neocortex/-/pipelines/26483 where the only project CI needed is https://bbpgitlab.epfl.ch/hpc/sim/models/neocortex/-/commit/6cf9066efd0444782ab83509644c93561c05d3bc
    
    For this to work a new Access Token was created: CI_WRITE_TOKEN, given the name CI_Automation
    
    ----
    Original commit: 295194b36ff6e6f467e8b16929e2fb4876634f2f

--- update_models.sh
+++ update_models.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+declare -A MODEL_BRANCH
+MODEL_BRANCH["common"]=${MODELS_COMMON_BRANCH:-main}
+MODEL_BRANCH["neocortex"]=${NEOCORTEX_BRANCH:-main}
+MODEL_BRANCH["hippocampus"]=${HIPPOCAMPUS_BRANCH:-main}
+MODEL_BRANCH["thalamus"]=${THALAMUS_BRANCH:-main}
+MODEL_BRANCH["mousify"]=${MOUSIFY_BRANCH:-main}
+
+
 echo "==> Fetching latest models..."
 updated_modules=""
 update_details=""
@@ -12,17 +20,19 @@ echo " -> Sync (Submodule update)"
 git submodule update
 
 for d in */; do
-    echo " -> Handling repo $d"
-    pushd $d > /dev/null
-    fetch_log=$(git fetch origin main 2>&1)
-    if [ $? -ne 0 ]; then
-        echo "Error fetching. Log: $nl $fetch_log"
-        exit 1
+    model_name="${d%/}"
+    if [ ${d::1} == "_" ]; then continue
     fi
+    echo " -> Handling repo: $model_name"
+    pushd $d > /dev/null
+    fetch_log=$(git fetch origin ${MODEL_BRANCH[$model_name]} 2>&1) || {
+        echo "Error fetching. Log: $nl$fetch_log"
+        exit 1
+    }
     if [ $(git rev-list HEAD..FETCH_HEAD --count) -gt 0 ]; then
-        commit_info="$d: $(git log -1 FETCH_HEAD --oneline --no-decorate)"
-        updated_modules="$updated_modules $d"
-        update_details="$update_details $nl $commit_info"
+        commit_info="$model_name: $(git log -1 FETCH_HEAD --oneline --no-decorate)"
+        updated_modules="$updated_modules $model_name"
+        update_details="$update_details $nl - $commit_info"
         git reset --hard FETCH_HEAD
     fi
     rm -rf common
@@ -38,7 +48,7 @@ if [ "$updated_modules" ]; then
     outp=$(git diff --cached --name-only)
     if [ "$outp" ]; then
         echo " -> New commits found. Creating new bump commit"
-        git commit -m "Bump models: $updated_modules $nl $update_details"
+        git commit -m "Bumping models: $updated_modules $nl $update_details"
         echo " -> Modules were updated. Commit created! Done."
     else
         echo " -> Already up to date. Done"
