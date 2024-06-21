0981a2c4071864ed31e53022fbf16b10a1e0453e
Weina Ji <weina.ji@epfl.ch>
2024-06-21 12:31:58 +0200

    update vecevent.mod in the test

--- common/test/run_all.sh
+++ common/test/run_all.sh
@@ -5,7 +5,7 @@ which nrniv || (echo "Please load Neuron" && exit 1)
 cd "$(dirname "${BASH_SOURCE[0]}")"  # cd to this dir
 
 pushd glusynapse
-  nrnivmodl ../../mod/VecStim.mod ../../mod/GluSynapse.mod  # Compile mod
+  nrnivmodl ../../mod/vecevent.mod ../../mod/GluSynapse.mod  # Compile mod
   nosetests -v test_transmission.py
   nosetests -v test_ltpltd.py
 popd
